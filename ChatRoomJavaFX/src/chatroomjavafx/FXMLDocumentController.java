7366f12eccbe996d873bbace68db84ad5aeed461
matthew mo <matthew mo>
2017-03-27 11:46:58 +0100

    Added in basic socket functionality

--- ChatRoomJavaFX/src/chatroomjavafx/FXMLDocumentController.java
+++ ChatRoomJavaFX/src/chatroomjavafx/FXMLDocumentController.java
@@ -5,8 +5,15 @@
  */
 package chatroomjavafx;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -39,8 +46,25 @@ public class FXMLDocumentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        
+        try {
+            // TODO
+            String hostName = "localhost";
+            int portNumber = 80;
+            
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            
+            String userInput;
+            while((userInput = stdIn.readLine()) != null)
+            {
+                chatRoom.setText(userInput);
+                chatRoom.setText("echo: " + in.readLine());
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
     
 }
