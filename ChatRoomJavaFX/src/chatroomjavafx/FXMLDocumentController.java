ac8b8be20420bf89f40ade307e1076b9eef47b14
matthew mo <matthew mo>
2017-03-27 11:13:07 +0100

    added in enter user name / enter chat room functionality

--- ChatRoomJavaFX/src/chatroomjavafx/FXMLDocumentController.java
+++ ChatRoomJavaFX/src/chatroomjavafx/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,18 +21,26 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button send;
+
+    @FXML
+    private TableColumn<?, ?> users;
+
+    @FXML
+    private TextField chatMessage;
+
+    @FXML
+    private TextField chatRoom;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
     
 }
